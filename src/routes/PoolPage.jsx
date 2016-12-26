e58dea3e71697b66c6280a29e9091faedc4d32fb
WongBen <wong915@gmail.com>
2016-12-26 18:47:33 +0800

    增加postcss

--- src/routes/PoolPage.jsx
+++ src/routes/PoolPage.jsx
@@ -1,5 +1,5 @@
 /* eslint import/extensions: 0, no-dupe-keys: 0, no-mixed-operators: 0*/
-import { WhiteSpace, WingBlank, Button, List } from 'antd-mobile';
+import { WhiteSpace, WingBlank, Button, List, Icon, Card } from 'antd-mobile';
 import React, { PropTypes } from 'react';
 import styles from './PoolPage.less';
 
@@ -7,15 +7,19 @@ const Item = List.Item;
 const Brief = Item.Brief;
 
 const PoolPage = ({ pool, loading }) => (
-  <div className={styles.normal}>
-    <List>
-      <Item extra="没有箭头" arrow="empty">标题文字</Item>
-      <Item extra="没有箭头" arrow="empty">标题文字</Item>
+  <div>
+    <List className="my-list">
+      <Item wrap><Icon type="phone" /></Item>
+      <Item><span>评分 : </span><Icon type="star" /> <Icon type="star" /> <Icon type="star" /> <Icon type="star" /> </Item>
+      <Card><div className={styles.wifi_icon} /></Card>
+      <Card><img className={styles.image_test} src="/assets/wifi.png" alt="" /></Card>
+      <Card><img className={styles.image_test} src="http://img.xskhome.com/pic/2016/news/20160524/191010_3721317444.png" alt="" /></Card>
+      <Item extra="没11有箭头" arrow="empty">标题文字</Item>
     </List>
     <div className="button-container">
       <WhiteSpace size="xs" />
       <WingBlank size="md">
-        <Button className="btn" type="primary" size="small">选择票券</Button>
+        <Button className="btn" type="primary" size="large">选择票券</Button>
       </WingBlank>
     </div>
   </div>

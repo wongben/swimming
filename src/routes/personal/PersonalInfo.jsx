37468cdce7b1e5152f447b80a51f147677b883f7
wufu <wufu@192.168.2.128>
2017-01-09 18:43:18 +0800

    commit personalInfo

---
+++ src/routes/personal/PersonalInfo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Table, List, InputItem, Switch, Stepper, Slider, Button } from 'antd-mobile';
+import FilletImage from '../../components/Common/FilletImage';
+import styles from './PersonalInfo.less';
+
+const Item = List.Item;
+const Brief = Item.Brief;
+
+const PersonalInfo = () => {
+  {/* get请求数据 */}
+  const personalObj = {
+  	name:"游泳达人",
+  	phone:"15021123142",
+  	imageUrl: "http://img.release.1yd.me/Fnq3JmmOan-yAHtJHk-n9-o3Qqbr"
+  };
+
+  return (
+	<div className={styles.div_body}>
+      <List className="my-list">
+        <Item extra={<div className={styles.div_top}><FilletImage width="100px" height="100px" imageUrl={personalObj.imageUrl} /></div>}>头像</Item>
+        <Item extra={<div>{personalObj.name}</div>}>昵称</Item>
+        <Item extra={<div>{personalObj.phone}</div>}>手机号</Item>
+        <Item extra={<div>修改</div>}>登录密码</Item>
+      </List>
+   	</div>
+)};
+
+export default PersonalInfo;

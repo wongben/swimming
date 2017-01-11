666e6accd3aec1a78a12f555d298ae453e8c0fc5
Zenguoqing <zengguoqing@1yd.me>
2017-01-11 13:50:15 +0800

    no message

---
+++ src/components/Discount/index.jsx
@@ -0,0 +1,7 @@
+/*
+ * @Author: warwick.ZengGuoqing 
+ * @Date: 2017-01-11 13:22:16 
+ * @Last Modified by:   warwick.ZengGuoqing 
+ * @Last Modified time: 2017-01-11 13:22:16 
+ */
+export {default} from './CoachTop'
